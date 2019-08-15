5d9dc28241c5e1069db4aeafe70cb350e50970c2
Kun <1ambda@users.noreply.github.com>
2019-08-15 16:40:02 +0900

    fix: Invalid k8s manifest name for EBS

--- k8s-jupyter-hub/jupyter.helm-prepare.sh
+++ k8s-jupyter-hub/jupyter.helm-prepare.sh
@@ -8,4 +8,4 @@ RELEASE=jupyterhub
 NAMESPACE=jupyter-production
 
 kubectl apply -f ./k8s.efs-volume.yaml
-kubectl apply -f ./k8s.efs-storage-class.yaml
+kubectl apply -f ./k8s.ebs-storage-class.yaml
